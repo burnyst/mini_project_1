caee5d9d4f6ee7029197da80ae31682a08270872
youngjoon kim <bigdream9609@gmail.com>
2021-12-10 22:02:32 +0900

    refactor: JDBCUtil.java
    Changed to create an object with a large creation cost only once
    Import spring.datasource from application.properties
    Delete unused code
    Other code cleanup

--- src/main/java/com/fastcampus/jblog/common/JDBCUtil.java
+++ src/main/java/com/fastcampus/jblog/common/JDBCUtil.java
@@ -1,19 +1,37 @@
 package com.fastcampus.jblog.common;
 
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class JDBCUtil {
+
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
+
+    static {
+        try {
+            // JDBC 드라이버 로드
+            DriverManager.registerDriver(new org.h2.Driver());
+
+            // JDBC 커넥션 정보 가져오기
+            Properties properties = new Properties();
+            properties.load(new FileReader("src/main/resources/application.properties"));
+            URL = properties.getProperty("spring.datasource.hikari.jdbc-url");
+            USER = properties.getProperty("spring.datasource.hikari.username");
+            PASSWORD = properties.getProperty("spring.datasource.hikari.password");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static Connection getConnection() {
         Connection conn = null;
         try {
-            DriverManager.registerDriver(new org.h2.Driver());
-//            Class.forName("org.h2.Driver");
-            conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "");
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -23,7 +41,7 @@ public class JDBCUtil {
     public static void close(Statement stmt, Connection conn) {
         try {
             stmt.close();
-        } catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
             stmt = null;
@@ -31,36 +49,10 @@ public class JDBCUtil {
 
         try {
             conn.close();
-        } catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
             conn = null;
         }
     }
-
-	public static void close(ResultSet rs, PreparedStatement stmt, Connection conn) {
-		try {
-            rs.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            rs = null;
-        }
-		
-		try {
-            stmt.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            stmt = null;
-        }
-
-        try {
-            conn.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            conn = null;
-        }
-	}
 }
